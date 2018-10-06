a8b0794c661109f008a393fbe08cc23d1c6208ab
halfcadence <andrew@halfcadence.io>
2018-10-06 15:15:32 -0700

    commit build

---
+++ my-extension/build/content_script.js
@@ -0,0 +1,37 @@
+alert("here");
+
+function loadjscssfile(filename, filetype){
+    if (filetype=="js"){ //if filename is a external JavaScript file
+        var fileref=document.createElement('script')
+        fileref.setAttribute("type","text/javascript")
+        fileref.setAttribute("src", filename)
+    }
+    else if (filetype=="css"){ //if filename is an external CSS file
+        var fileref=document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", filename)
+    }
+    if (typeof fileref!="undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+
+// loadjscssfile("app.js", "js") //dynamically load and add this .js file
+var docRef=document.createElement("div")
+docRef.setAttribute("id", "quizzes");
+document.getElementsByTagName("body")[0].appendChild(docRef)
+// $("#quizzes").load("index_1.html");
+$.get(chrome.extension.getURL('/index.html'), function(data) {
+    // $(data).appendTo('body');
+    // console.log("data", data)
+    console.log(chrome.extension.getURL('/index.html'))
+    var url = chrome.extension.getURL('/index.html').replace("index.html", "")
+    //document.innerHTML = data
+    data = data.replace(/static/g, url+"static/")
+
+    console.log("data", data)
+
+    $("#quizzes").html(data)
+    // Or if you're using jQuery 1.8+:
+    // $($.parseHTML(data)).appendTo('body');
+});
