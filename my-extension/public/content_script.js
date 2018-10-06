c8f23b5a19ddc6523f15c48e074595e0183aec21
yichiang <yi_chiang@Outlook.com>
2018-10-06 15:48:34 -0700

    add code

--- my-extension/public/content_script.js
+++ my-extension/public/content_script.js
@@ -20,19 +20,27 @@ function loadjscssfile(filename, filetype){
 var docRef=document.createElement("div")
 docRef.setAttribute("id", "quizzes");
 document.getElementsByTagName("body")[0].appendChild(docRef)
+//document.getElementsByTagName("head")[0].appendChild('<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">')
+$('head').append('<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">');
+
+//$("#quizzes").html('<iframe src="http://localhost:8080/index.html"></iframe>')
+docRef.innerHTML = '<iframe src="http://localhost:8080/index.html"></iframe>'
 // $("#quizzes").load("index_1.html");
-$.get(chrome.extension.getURL('/index.html'), function(data) {
-    // $(data).appendTo('body');
-    // console.log("data", data)
-    console.log(chrome.extension.getURL('/index.html'))
-    var url = chrome.extension.getURL('/index.html').replace("index.html", "")
-    //document.innerHTML = data
-    data = data.replace(/\/static/g, url+"static")
-    data = data.replace(/\app.js/g, url+"app.js")
-
-    console.log("data", data)
-
-    $("#quizzes").html(data)
-    // Or if you're using jQuery 1.8+:
-    // $($.parseHTML(data)).appendTo('body');
-});
+// $.get(chrome.extension.getURL('/index.html'), function(data) {
+//     // $(data).appendTo('body');
+//     // console.log("data", data)
+//     console.log(chrome.extension.getURL('/index.html'))
+//     var url = chrome.extension.getURL('/index.html').replace("index.html", "")
+//     //document.innerHTML = data
+//     data = data.replace(/\/static/g, url+"static")
+//     data = data.replace(/\app.js/g, url+"app.js")
+//
+//     console.log("data", data)
+//
+//     // $("#quizzes").html(data)
+//
+// $("#quizzes").html('<iframe src="https://www.w3schools.com"></iframe>')
+//
+//     // Or if you're using jQuery 1.8+:
+//     // $($.parseHTML(data)).appendTo('body');
+// });
