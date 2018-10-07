1422ed16fcc6e9e3b481f05ebbb8ccf0d6e07bcf
halfcadence <andrew@halfcadence.io>
2018-10-06 17:26:13 -0700

    mergee

--- my-extension/src/App.js
+++ my-extension/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Question from './components/Question';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         App
-
         <input id="myinput" />
-
+        <Question questionNumber="1" />
       </div>
     );
   }
---
+++ my-extension/src/components/Question.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {Container, List, Button} from 'semantic-ui-react';
+
+class Question extends Component {
+  render(props) {
+    return (
+      <Container>
+        <h2>
+          {props.questionNumber} What is the worst fruit in a fruit cup?
+        </h2>
+        <List>
+          <List.Item><Button>Canteloupe</Button></List.Item>
+          <List.Item><Button>Watermelon</Button></List.Item>
+          <List.Item><Button>Orange</Button></List.Item>
+          <List.Item><Button>Raspberry</Button></List.Item>
+        </List>
+      </Container>
+    );
+  }
+};
+
+export default Question;
